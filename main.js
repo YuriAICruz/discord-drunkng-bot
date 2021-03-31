3db4a90849ebb07540b9f5663d25496d4a077f93
Yuri Cruz <yuriaicruz@gmail.com>
2021-03-31 00:35:54 -0300

    game logic

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ const client = new Discord.Client();
  */
 let lastMember;
 
+/**
+ * @type {string}
+ */
+let currentPassword;
+
+let gameRunning = false;
+let passwordSize = 5;
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -17,7 +25,13 @@ client.on("message", (msg) => {
 
   const cmds = txtMsg.split(/\s+/);
 
-  if (cmds[0] != "game") return;
+  if (cmds[0] != "game") {
+    if (gameRunning) {
+      gameLogic(msg);
+      return;
+    }
+    return;
+  }
 
   const channel = msg?.member?.voice?.channel;
 
@@ -28,6 +42,9 @@ client.on("message", (msg) => {
   switch (cmds[1]) {
     case "start":
       start(msg, members, cmds.slice(2));
+      break;
+    case "end":
+      end(msg);
       break;
     case "help":
       help(msg);
@@ -56,21 +73,121 @@ async function help(msg) {
  * @param {Discord.Collection<string, Discord.GuildMember>} members
  * @param {Array<string>} cmds
  */
-function start(msg, members, cmds) {
+async function start(msg, members, cmds) {
+  if (gameRunning) {
+    msg.reply("O jogo ja está rolando.");
+    return;
+  }
   if (members.size <= 1) {
-    msg.reply("Não pode brincar sozinho");
+    msg.reply("Não pode brincar sozinho.");
     return;
   }
 
+  gameRunning = true;
+
+  const newMember = randomMember(members);
+
+  currentPassword = createPassword();
+
+  await respond(msg, newMember);
+
+  lastMember = newMember;
+}
+
+/**
+ *
+ * @param {Discord.GuildMember} members
+ * @param {Discord.GuildMember} newMember
+ * @returns
+ */
+function randomMember(members) {
   let newMember;
   do {
     const rnd = Math.floor(Math.random() * members.size);
     newMember = members.array()[rnd];
   } while (newMember == lastMember);
+  return newMember;
+}
 
+async function respond(msg, newMember) {
   msg.channel.send(`<@${newMember.id}>`);
+  msg.channel.send("3");
+  await sleep(1000);
+  msg.channel.send("2");
+  await sleep(1000);
+  msg.channel.send("1");
+  await sleep(1000);
+  msg.channel.send(`digite: **${currentPassword}**`);
+}
+
+/**
+ *
+ * @param {Discord.Message} msg
+ */
+function gameLogic(msg) {
+  if (msg.member != lastMember) {
+    return;
+  }
+
+  if (msg.cleanContent != currentPassword) {
+    msg.reply("Você errou!");
+    return;
+  }
+
+  next(msg);
+}
+/**
+ *
+ * @param {Discord.Message} msg
+ */
+async function end(msg) {
+  if (!gameRunning) return;
+
+  gameRunning = false;
+  lastMember = undefined;
+
+  msg.channel.send("jogo fechado");
+}
+
+/**
+ *
+ * @param {Discord.Message} msg
+ */
+async function next(msg) {
+  const channel = msg?.member?.voice?.channel;
+
+  if (!channel) {
+    msg.reply("você não está em um canal de voz");
+    return;
+  }
+
+  const members = getMembers(channel);
+
+  if (members.size <= 1) {
+    msg.reply("Não pode brincar sozinho.");
+    return;
+  }
+
+  const newMember = randomMember(members);
+
+  currentPassword = createPassword();
+
+  await respond(msg, newMember);
 
   lastMember = newMember;
+}
+
+/**
+ * @returns {string}
+ */
+function createPassword() {
+  const chars = "qwertyuiopasdfghjklçzxcvbnm1234567890^/*-+.@#$!%&()_=";
+  let word = [];
+  for (let i = 0; i < passwordSize; i++) {
+    word.push(chars[Math.floor(Math.random() * chars.length)]);
+  }
+
+  return word.join("");
 }
 
 /**
@@ -80,4 +197,14 @@ function start(msg, members, cmds) {
  */
 function getMembers(channel) {
   return channel.members.filter((x) => !x.user.bot);
+}
+
+/**
+ *
+ * @param {Number} ms
+ */
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
